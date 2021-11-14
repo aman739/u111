e991f6831fca60f621c4cf5da7fd261fedced627
aman739 <mr.dag50@gmail.com>
2021-11-14 18:28:20 +0300

    ДЗ 3.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,24 +25,6 @@ public class Main {
         System.out.println("Average " + sum / negativeNumbers);
 
 
-        int[] metamorphosis = {-4, -7, 1, 5, 8, 2};
-        int duration = 0;
-        for (int i = 0; i < metamorphosis.length; i++) {
-            for (int j = i + 1; j < metamorphosis.length; j++) {
-                if (metamorphosis[i] > metamorphosis[j]) {
-                    duration = metamorphosis[i];
-                    metamorphosis[i] = metamorphosis[j];
-                    metamorphosis[j] = duration;
-
-                }
-
-            }
-
-        }
-        System.out.println("Elements of the array sorted in the ascending order");
-        for (int i = 0; i < metamorphosis.length; i++) {
-            System.out.println(metamorphosis[i]);
-
 
         }
 
@@ -59,8 +41,8 @@ public class Main {
 
 
 
-        
-    }}
+
+    }
 
 
 
